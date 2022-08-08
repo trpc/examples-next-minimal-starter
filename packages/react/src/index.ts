7f528d2e9fb87c2e3c98e184dae4cbcd768f0936
Julius Marminge <julius0216@outlook.com>
2022-08-08 19:50:04 +0200

    chore: upgrade next-minimal-starter to v10 (#2169)
    Co-authored-by: KATT <alexander@n1s.se>

--- packages/react/src/index.ts
+++
@@ -1,6 +0,0 @@
-export * from '@trpc/client';
-
-export { createReactQueryHooks } from './createReactQueryHooks';
-export { createReactQueryHooksProxy } from './createReactQueryHooksProxy';
-export type { DecoratedProcedureUtilsRecord } from './internals/utilsProxy';
-export type { DecoratedProcedureRecord } from './createReactQueryHooksProxy';
